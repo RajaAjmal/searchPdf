f0dbde09e485c950e6abf78c9cc30af4e3d4e9cd
joe-smith-py <64045158+joe-smith-py@users.noreply.github.com>
2020-04-21 22:10:49 +0300

    Update searchPdf2.py
    update for multiprocessing

--- searchPdf2.py
+++ searchPdf2.py
@@ -1,41 +1,46 @@
-from PyPDF2 import utils
-import re
-import os, sys, subprocess
 import PyPDF2
+import re
+import os
+from multiprocessing import Pool
 
-def searchPdf(si,ch):
-	found = False
-	for foldername, subfolders, files in os.walk(ch):
-		for file in files:
-			if file[-3:] == 'pdf':
-				try:
-					object = PyPDF2.PdfFileReader(os.path.join(foldername, file))
-					numPages = object.getNumPages()
-					try:
-						for i in range (0,numPages):
-							pageObj = object.getPage(i)
-							text = pageObj.extractText()
-							textCase = text.lower()
-							resSearch = re.search(si,textCase)
-							if resSearch != None:
-								found = True
-								print(file)
-								break
-					except:
-						print ('Cannot extract text from ' + file)
-				except:
-					print (file + ' is corrupt')
-	print ('Search completed')
-	if found == False:
-		print ('Searched words not found')
-		
-searchedItem = input ('Enter search word: ')
-print ('1.	SC')
-print('2.	HSC')
-choice = int(input('Select a folder:  '))
+example = '/home/joe/PycharmProjects/PDFsearch'
+
+def find_pdfs(directory):
+    pdfs = []
+    for foldername, subfolder, files in os.walk(directory):
+        for file in files:
+            if '.pdf' in file:
+                pdfs.append(foldername + '/' + file)
+    return pdfs
+
+def search_pdf(critera):
+    search_term, path = critera
+    pdf = PyPDF2.PdfFileReader(path)
+    for n in range(0, pdf.getNumPages()):
+        page = pdf.getPage(n)
+        text = page.extractText().lower()
+        search = re.search(search_term, text)
+        if search != None:
+            print(path)
+            break
+
+
+
+term = input('Enter search word: ')
+choice = int(input('1.	SC\n2.	HSC \nSelect a folder:  '))
+
+thread_count = int(input('How many threads would you like to devote? '))
+
 if choice == 1:
-	cho = '/media/raja/316E396D3C478D8A/pdrv/computer science/SC/'
+    cho = '/media/raja/316E396D3C478D8A/pdrv/computer science/SC/'
 else:
-	cho = '/media/raja/316E396D3C478D8A/pdrv/computer science/HSC/'
+    cho = '/media/raja/316E396D3C478D8A/pdrv/computer science/HSC/'
 
-searchPdf (searchedItem,cho)
+pdfs = find_pdfs(cho)
+
+search_crit = []
+for pdf in pdfs:
+    search_crit.append((term, pdf))
+
+with Pool(thread_count) as p:
+        p.map(search_pdf, search_crit)
